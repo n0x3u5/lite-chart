8468941b568e3ee943ed17b4f4b8d85198b75f77
Nakshatra Mukhopadhyay <nakshatra.mukhopadhyay1994@gmail.com>
2017-10-30 00:42:19 +0530

    Axis working without APIs

--- src/axes/axis.js
+++ src/axes/axis.js
@@ -2,7 +2,6 @@ const TOP = 1,
       RIGHT = 2,
       BOTTOM = 3,
       LEFT = 4,
-      // EPSILON = 1e-6,
       translateX = x => `translate(${x + 0.5}, 0)`,
       translateY = y => `translate(0, ${y + 0.5})`,
       center = (scale) => {
@@ -26,20 +25,20 @@ class Axis {
     this.tickPadding = 3;
     this.k = this.orient === TOP || this.orient === LEFT ? -1 : 1;
     this.x = this.orient === LEFT || this.orient === RIGHT ? 'x' : 'y';
+    this.xInverse = this.orient === LEFT || this.orient === RIGHT ? 'y' : 'x';
     this.transform = this.orient === TOP || this.orient === BOTTOM ? translateX : translateY;
   }
 
   draw (painter) {
     if (this.tickValues == null) {
-      if (this.scale.getTicks) {
-        this.values = this.scale.getTicks(this.scale, this.tickArguments);
+      if (this.scale.ticks) {
+        this.values = this.scale.ticks(...this.tickArguments);
       } else {
         this.values = this.scale.getDomain();
       }
     } else {
       this.values = this.tickValues;
     }
-
     this.spacing = Math.max(this.tickSizeInner, 0) + this.tickPadding;
     this.range = this.scale.getRange();
     this.range0 = Number(this.range[0]) + 0.5;
@@ -49,18 +48,55 @@ class Axis {
       : number)(this.scale.copy());
 
     if (!this.container) {
-      this.container = painter.group();
+      this.container = painter.group().attr({
+        fill: 'none',
+        'font-size': 10,
+        // eslint-disable-next-line
+        'text-anchor': this.orient === RIGHT ? 'start' : this.orient === LEFT ? 'end' : 'middle'
+      });
     }
 
-    painter.path('M20,30,L,40,40');
+    if (!this.line) {
+      this.line = this.container.path()
+        .stroke({
+          color: '#000000',
+        })
+        .addClass('domain')
+        .style('shape-rendering', 'crispEdges');
+    }
 
-    // this.selection = context.selection ? context.selection() : context,
-    // this.path = selection.selectAll(".domain").data([null]),
-    // this.tick = selection.selectAll(".tick").data(values, scale).order(),
-    // this.tickExit = tick.exit(),
-    // this.tickEnter = tick.enter().append("g").attr("class", "tick"),
-    // this.line = tick.select("line"),
-    // this.text = tick.select("text");
+    this.line.plot(this.orient === LEFT || this.orient === RIGHT
+      ? `M ${0} ${this.range0} V ${this.range1}`
+      : `M ${this.range0} ${0} H ${this.range1}`);
+
+    this.ticks = this.values.map((value) => {
+      const pos = this.position(value),
+            tickGroup = this.container.group().addClass('tick').attr({
+              opacity: 1
+            });
+
+      tickGroup.line()
+        .attr(`${this.xInverse}1`, pos)
+        .attr(`${this.x}1`, 0)
+        .attr(`${this.xInverse}2`, pos)
+        .attr(`${this.x}2`, this.k * this.tickSizeInner)
+        .stroke({
+          color: '#000000'
+        })
+        .style('shape-rendering', 'crispEdges');
+
+      tickGroup.text(value.toString())
+        .attr(this.x, this.k * this.spacing)
+        .attr(this.xInverse, pos)
+        .fill({
+          color: '#000000'
+        })
+        .lines()
+        // eslint-disable-next-line
+        .attr('dy', this.orient === TOP ? '0em' : this.orient === BOTTOM ? '0.71em' : '0.32em');
+
+      return tickGroup;
+    });
   }
 }
 
